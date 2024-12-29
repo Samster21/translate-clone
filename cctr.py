11b5a89c6bf4b881c591f511556a060a047f735e
Isaac Samuel T <isaacsamuel2003@gmail.com>
2024-12-29 19:51:03 +0530

    Updated logic

--- cctr.py
+++ cctr.py
@@ -1,61 +1,55 @@
-import argparse
+import argparse, string
 
 parser = argparse.ArgumentParser()
-parser.add_argument("fromm", type=str)
-parser.add_argument("to", type=str)
-parser.add_argument("--operation", choices=["s"], default="s")
-
+parser.add_argument("set_1", type=str)
+parser.add_argument("set_2", type=str)
+parser.add_argument("--operation", choices=["s"])
 args = parser.parse_args()
 
 
-a = str(input("Please enter a text: "))
+def identify(strng):
+    strng = list(strng)
 
-#type casting the string into a list
-a = list(a)
-
-
-#split arguments incase of a range of characters (i.e., A-Z)
-if (len(args.fromm), len(args.to)) == (3,3):
-
-    #Get the range of characters 
-    fromm = args.fromm.split('-')
-    to = args.to.split('-')
+    #If given a range of characters
+    if len(strng) == 3 and strng[1] == "-":
+        return [chr(i) for i in range (ord(strng[0]), ord(strng[2])+1)]
     
-    #Creates a list of the characters in both ranges
-    range_1 = [chr(i) for i in range(ord(fromm[0]), ord(fromm[1])+1)]
-    range_2 = [chr(i) for i in range(ord(to[0]), ord(to[1])+1)]
+    #If given uppercase
+    if strng == "[:upper:]":
+        return list(string.ascii_uppercase)
+    
+    #If given lowercase
+    if strng == "[:lower:]":
+        return list(string.ascii_lowercase)
+    
+    #For other than this case
+    return [strng[i] for i in range(len(strng))]
+    
 
-    for i in range(len(a)):
+def translate(user_input:str, set1:str, set2:str):
+
+    user_input = list(user_input)
+
+    range_1 = identify(set1)
+    range_2 = identify(set2)
+
+    for i in range(len(user_input)):
 
         #See if the current element is in the first range. For example, if the (from) range is [A-Z], then the 'x' in "Axe" should not be translated.
-        if a[i] in range_1:
+        if user_input[i] in range_1:
 
             #Find the position of the current character in the range. For example, the 'b' in Bob lies in index 1 of [a-z]
-            position = range_1.index(a[i])
-
-            #Edge case where the second set is smaller than the first, prevents index error
-            #Implement try
-            
-            # if temp >= len(range_2):
-            #     a[i] = range_2[len(range_2)-1]
-            # else:
-            #     a[i] = range_2[temp]
+            position = range_1.index(user_input[i])
 
             try:
-                a[i] = range_2[position]
+                user_input[i] = range_2[position]
+            
+            #Edge case where the second set is smaller than the first, prevents index error
             except IndexError:
-                a[i] = range_2[len(range_2)-1]
+                user_input[i] = range_2[len(range_2)-1]
 
+    return ''.join(user_input)
 
-    a = "".join(a)
-    print (a)    
+a = input("Please enter a text: ")
 
-if args.operation == "s":
-    for i in range(len(a)):
-        if a[i] == args.fromm:
-            a[i] = args.to
-
-    a = ''.join(a)
-    print(a)
-
-    
+print(translate(user_input=a, set1=args.set_1, set2=args.set_2))
